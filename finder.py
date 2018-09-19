60345c132553ae0fe71667e5e6664bac98485e56
Jarod Gowgiel <jroddg@gmail.com>
2018-09-19 15:51:05 -0400

    Updated to support MemCachier on the server

--- finder.py
+++ finder.py
@@ -1,12 +1,41 @@
 from flask import Flask, render_template, request
-from werkzeug.contrib.cache import MemcachedCache
+from flask_caching import Cache
 
+import os
 import requests
 import datetime
 
 app = Flask(__name__)
-cache = MemcachedCache(['127.0.0.1:11211'])
+cache = Cache()
 
+# Cache configuration from https://devcenter.heroku.com/articles/memcachier#flask
+cache_servers = os.environ.get('MEMCACHIER_SERVERS')
+if cache_servers == None:
+    # Fall back to simple in memory cache (development)
+    cache.init_app(app, config={'CACHE_TYPE': 'simple'})
+else:
+    cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
+    cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
+    cache.init_app(app,
+        config={'CACHE_TYPE': 'saslmemcached',
+                'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
+                'CACHE_MEMCACHED_USERNAME': cache_user,
+                'CACHE_MEMCACHED_PASSWORD': cache_pass,
+                'CACHE_OPTIONS': { 'behaviors': {
+                    # Faster IO
+                    'tcp_nodelay': True,
+                    # Keep connection alive
+                    'tcp_keepalive': True,
+                    # Timeout for set/get requests
+                    'connect_timeout': 2000, # ms
+                    'send_timeout': 750 * 1000, # us
+                    'receive_timeout': 750 * 1000, # us
+                    '_poll_timeout': 2000, # ms
+                    # Better failover
+                    'ketama': True,
+                    'remove_failed': 1,
+                    'retry_timeout': 2,
+                    'dead_timeout': 30}}})
 ROOT_API = 'https://tuftsdiningdata.herokuapp.com/menus/'
 
 # At most, look two weeks in advance
